458db6de05919f9f08e161f57724ecde2e87f584
Ian Penrod <ianpenrod@gmail.com>
2025-07-28 21:27:10 -0500

    bug: Enable CORS for local development

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import tasksRouter from "./routes/tasks";
 
@@ -7,6 +8,13 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
